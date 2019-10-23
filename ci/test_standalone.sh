7e8d86fb6ddcc4a02ab3017c99838b168d5a72b5
Michał Sitko <pierwszy1@gmail.com>
2019-10-23 23:23:30 +0200

    wip

--- ci/test_standalone.sh
+++ ci/test_standalone.sh
@@ -9,34 +9,41 @@ function write_scala_ssh_config() {
 
   mkdir ~/.scala-ssh
   echo localhost > ~/.scala-ssh/.testhost
+  FULLPATH="$(pwd)/id_ed25519"
 
   cat <<EOF >  ~/.scala-ssh/localhost
 login-type  = keyfile
 username    = root
-keyfile     = id_ed25519
+keyfile     = $FULLPATH
 port        = $PORT
 EOF
 }
 
-rm -rf ci/volumes
+cd ci
 
 ssh-keygen -t ed25519 -f id_ed25519 -N "" -q
 
-mkdir -p ci/volumes/client
-cp id_ed25519.pub ci/volumes/
-mv ci/volumes/id_ed25519.pub ci/volumes/authorized_keys
+#mkdir -p volumes/sshd
+#cp id_ed25519.pub volumes/sshd
+#mv volumes/sshd/id_ed25519.pub volumes/sshd/authorized_keys
 
-docker-compose -f ci/docker-compose.yml pull sshd
-docker-compose -f ci/docker-compose.yml up sshd &
+#ls -al volumes
+#cat volumes/authorized_keys
+
+docker pull rastasheep/ubuntu-sshd:16.04
+docker run -d -P --name test_sshd rastasheep/ubuntu-sshd:16.04
 
 docker ps -a
 
 # Just to be sure sshd started
-sleep 5
+sleep 9
 
 docker ps -a
 docker --version
-docker-compose --version
+
+docker cp id_ed25519.pub test_sshd:/root/.ssh/authorized_keys
+docker exec test_sshd chown root:root /root/.ssh/authorized_keys
+
 CONTAINER_ID=`docker ps -a -q`
 echo "sshd container_id detected: $CONTAINER_ID"
 SSHD_PORT=`docker inspect --format '{{ (index (index .NetworkSettings.Ports "22/tcp") 0).HostPort }}' "$CONTAINER_ID"`
@@ -45,4 +52,5 @@ write_scala_ssh_config $SSHD_PORT
 
 ssh-keyscan -t ed25519 -p "$SSHD_PORT" localhost >>~/.ssh/known_hosts
 
+cd ..
 sbt test

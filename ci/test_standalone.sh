378080eb9a1bbb3de596abb2b433be18a5ef9b96
Michał Sitko <pierwszy1@gmail.com>
2019-10-24 13:08:30 +0200

    refactorig

--- ci/test_standalone.sh
+++ ci/test_standalone.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # enable job control
 set -m
+
+PRIVATE_KEY_FILENAME="id_ed25519"
+PUBLIC_KEY_FILENAME="id_ed25519.pub"
+DOCKER_IMAGE_NAME="rastasheep/ubuntu-sshd:16.04"
 
 function write_scala_ssh_config() {
   local PORT="$1"
@@ -9,7 +13,7 @@ function write_scala_ssh_config() {
 
   mkdir ~/.scala-ssh
   echo localhost > ~/.scala-ssh/.testhost
-  FULLPATH="$(pwd)/id_ed25519"
+  FULLPATH=`realpath $PRIVATE_KEY_FILENAME`
 
   cat <<EOF >  ~/.scala-ssh/localhost
 login-type  = keyfile
@@ -19,38 +23,33 @@ port        = $PORT
 EOF
 }
 
-cd ci
+ssh-keygen -t ed25519 -f "$PRIVATE_KEY_FILENAME" -N "" -q
 
-ssh-keygen -t ed25519 -f id_ed25519 -N "" -q
-
-#mkdir -p volumes/sshd
-#cp id_ed25519.pub volumes/sshd
-#mv volumes/sshd/id_ed25519.pub volumes/sshd/authorized_keys
-
-#ls -al volumes
-#cat volumes/authorized_keys
-
-docker pull rastasheep/ubuntu-sshd:16.04
-docker run -d -P --name test_sshd rastasheep/ubuntu-sshd:16.04
-
-docker ps -a
+docker pull "$DOCKER_IMAGE_NAME"
+docker run -d -P --name test_sshd "$DOCKER_IMAGE_NAME"
 
 # Just to be sure sshd started
-sleep 9
+RETRIES_LEFT=15
+COMMAND_STATUS=1
+until { [ $COMMAND_STATUS -eq 0 ] || [ $RETRIES_LEFT -eq 0 ]; }; do
+  echo "checking if sshd is up: $RETRIES_LEFT"
+  docker ps -a | grep test_sshd
+  COMMAND_STATUS=$?
+  sleep 2
+  let RETRIES_LEFT=RETRIES_LEFT-1
+done
 
-docker ps -a
-docker --version
-
-docker cp id_ed25519.pub test_sshd:/root/.ssh/authorized_keys
+docker cp "$PUBLIC_KEY_FILENAME" test_sshd:/root/.ssh/authorized_keys
 docker exec test_sshd chown root:root /root/.ssh/authorized_keys
 
-CONTAINER_ID=`docker ps -a -q`
-echo "sshd container_id detected: $CONTAINER_ID"
-SSHD_PORT=`docker inspect --format '{{ (index (index .NetworkSettings.Ports "22/tcp") 0).HostPort }}' "$CONTAINER_ID"`
+# returns e.g. 0.0.0.0:32875
+SSHD_HOST_PORT=`docker port test_sshd 22`
+
+# returns e.g. 32875, uses https://stackoverflow.com/a/3162500/429311
+SSHD_PORT=${SSHD_HOST_PORT##*:}
 echo "sshd ephemeral port detected: $SSHD_PORT"
 write_scala_ssh_config $SSHD_PORT
 
 ssh-keyscan -t ed25519 -p "$SSHD_PORT" localhost >>~/.ssh/known_hosts
 
-cd ..
 sbt test
